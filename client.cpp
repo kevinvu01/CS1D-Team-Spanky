c0382a8869e30a631553b27052893370029d1506
haitrangnguyen <47197572+haitrangnguyen@users.noreply.github.com>
2019-02-04 16:51:50 -0800

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,9 +1,64 @@
 #include <iostream>
-using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include <string>
 
-int main() {
+using namespace std;
+
+
+int main()
+{
+	bool cont=true;
+	char choice;
 	
-	system("pause");
-	return 0;
+	do
+	{	
+		system("cls");
+		cout<<"Team Spanky: Foodie Extravaganza"<<endl<<endl;
+		
+		cout<<"<1>	View Restaurants and Menus"<<endl;
+		cout<<"<2>	Plan a Trip"<<endl;
+		cout<<"<3>	Edit Restaurants and Menus"<<endl;
+		cout<<"<Q>	Quit"<<endl;
+		cout<<"Enter choice option: ";
+		cin>>choice;
+		cin.clear();
+		
+		switch (choice)
+		{
+			case '1':
+				{	
+					cout<<"View"<<endl;
+					//viewData();
+					break;
+				}
+			case '2':
+				{
+					cout<<"Plan"<<endl;
+					//planTrip();
+					break;
+				}
+			case '3':
+				{
+					cout<<"login"<<endl;
+					//if (loggedIN())
+					//	adminEdit();
+					//else
+					//	cout<<"Login Error. Please try again."<<endl;
+					break;
+				}
+			case 'q':
+				{	
+					cout<<"quit"<<endl;
+					cont=false;
+					break;
+				}
+			case 'Q':
+				{
+					cout<<"quit"<<endl;
+					cont=false;
+					break;
+				}
+		}
+		
+		system("pause");
+	}while (cont==true);
 }
