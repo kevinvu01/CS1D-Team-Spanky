ab53c3461d1ea6f1a4be270f97b71d86b991828a
yycyuchen <46766273+yycyuchen@users.noreply.github.com>
2019-02-09 17:39:09 -0800

    Update restaurantType.h

--- restaurantType.h
+++ restaurantType.h
@@ -1,6 +1,18 @@
+#ifndef H_menuItemType
+#define H_menuItemType
+
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 class restaurantType{
-	
+public: 
+  
+private:
+
+
 };
+
+
+#endif
