8f767441b5f7a316d9e3b23c3afbb9a702a35b60
David D <46694467+d11deboer@users.noreply.github.com>
2019-02-10 18:25:10 -0800

    Update restaurantType.h

--- restaurantType.h
+++ restaurantType.h
@@ -1,17 +1,26 @@
-#ifndef H_menuItemType
-#define H_menuItemType
+#ifndef H_RESTAURANTTYPE
+#define H_RESTAURANTTYPE
 
+#include "DLinkedList.h"
 #include <iostream>
+
 #include <string>
 
 using namespace std;
 
 class restaurantType{
 public: 
-  
+  restaurantType();
+  void save();
+  void inFile();
+  void printMenu();
+  double getDistance();
+  string getName();
+  void printCart();
 private:
-
-
+  DLinkedList <MenuItemType> menu;
+  DistanceType map;
+  CartType cart;
 };
 
 
