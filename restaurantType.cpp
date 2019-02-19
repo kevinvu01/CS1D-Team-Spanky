5db6136c85c665a3e38728034a6ce349922512dc
yycyuchen <46766273+yycyuchen@users.noreply.github.com>
2019-02-18 20:49:30 -0800

    Update restaurantType.cpp

--- restaurantType.cpp
+++ restaurantType.cpp
@@ -1,1 +1,37 @@
 #include "restaurantType.h"
+
+restaurantType::restaurantType(){
+
+}
+
+restaurantType:: save(){
+
+}
+
+restaurantType::inFile(){
+
+}
+
+void restaurantType::printMenu(){
+
+}
+
+void restaurantType::setName(string x){
+  name = x;
+}
+
+void restaurantType::setDistance(double x){
+  distance = x;
+}
+
+string restaurantType::getName(){
+   return name;
+}
+
+double restaurantType::setDistance(){
+  return distance;
+}
+
+void restaurantType::printCart(){
+
+}
