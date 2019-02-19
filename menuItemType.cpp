23fc1c52fe30b799377502eb7445d4b900ade763
yycyuchen <46766273+yycyuchen@users.noreply.github.com>
2019-02-18 20:31:26 -0800

    Update menuItemType.cpp

--- menuItemType.cpp
+++ menuItemType.cpp
@@ -1,1 +1,21 @@
 #include "menuItemType.h"
+
+menuItemType::menuItemType(){
+
+}
+
+menuItemType::menuItemType(menuItemType &) const{
+
+}
+
+void menuItemType::setName(string x){
+name = x;
+}
+
+string menuItemType::getName(){
+  return name;
+}
+
+void menuItemType:: print(){
+
+}
