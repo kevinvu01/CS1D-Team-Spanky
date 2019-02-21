feeae5396568118796feae04b8e1c6878dfde4e5
David D <46694467+d11deboer@users.noreply.github.com>
2019-02-20 16:17:26 -0800

    Update distanceType.cpp

--- distanceType.cpp
+++ distanceType.cpp
@@ -1,9 +1,93 @@
 #include "distanceType.h"
 
-void distanceType:: setDistance(double x){
-    distance = x;
+//DISTANCEPAIR CONSTRUCTOR and things
+distPair::distPair()
+{
+	code = 0;
+	value = 0;
 }
 
-double distanceType::getDistance{
-    return distance;
+bool distPair::operator>=(const distPair& other)
+{
+	bool greater = false;
+	
+	if(value >= other.value)
+		greater = true;
+	
+	return greater;
 }
+
+bool distPair::operator!=(const distPair& other)
+{
+	bool inequality = false;
+	
+	if(value != other.value)
+		inequality = true;
+	
+	return inequality;
+}
+
+bool distPair::operator==(const distPair& other)
+{
+	bool equality = false;
+	
+	if(value == other.value)
+		equality = true;
+	
+	return equality;
+}
+
+//static initialization...oofda
+double distanceType::totalDist = 0;
+
+
+//DISTANCETYPE MEMBER FUNCTIONS>>>
+
+
+distanceType::distanceType()
+			:distanceList()
+{
+	numRestaurants = 12;
+	distToSC       = 0;
+}
+
+distanceType::distanceType(const orderedLinkedList &other, int num, double d)
+			:distanceList(other)
+{
+	numRestaurants = num;
+	distToSC       = d;
+}
+	
+const distanceType& distanceType::operator=(const distanceType& other)
+{
+	numRestaurants = other.numRestaurants;
+	distToSC       = other.distToSC;
+	distanceList   = other.distanceList;
+}
+
+	
+double distanceType::getDistToSC()
+{
+	return distToSC;
+}
+
+void distanceType::setDistToSC(double newDist)
+{
+	distToSC = newDist;
+}
+
+void distanceType::printTotalDist()
+{
+	cout << "Total Distance: " << totalDist << endl;
+}
+
+void distanceType::save()
+{
+	ofstream fout;
+	fout.open("data.txt\0");
+	
+	//fout << stuff
+	
+	fout.close();
+}
+
