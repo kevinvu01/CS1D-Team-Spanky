404ed9404c356f92dfb8fae4ac269b56c069ac46
David D <46694467+d11deboer@users.noreply.github.com>
2019-02-25 11:03:06 -0800

    Update cartType.h

--- cartType.h
+++ cartType.h
@@ -1,6 +1,6 @@
 
-#ifndef H_cartType
-#define H_cartType
+#ifndef H_CARTTYPE
+#define H_CARTTYPE
 
 #include "menuItemType.h"
 #include "restaurantType.h"
@@ -8,23 +8,27 @@
 #include <iomanip>
 #include <string>
 
-
 using namespace std;
 
 class cartType: public restaurantType
 {
 	using restaurantType::cart;
+	using restaurantType::revenue;
 public: 
   cartType();
-  cartType(vector <menuItemType>, double);
+  cartType(vector <menuItemType>, const distanceType&, const vector<menuItemType> &, int restCode, string name, double rev, double tot);
   
   const cartType& operator=(const cartType &);
   
   double getTotal();
-  void printContents();
-  void selectionMenu(const restaurantType &);
-  void checkout(const restaurantType &);
   
+  //printCart - prints the items of the cart with numbers
+  //Postcondition - outputs to screen the cart with heading and numbers
+  void printCart();
+  
+  void selectionMenu();
+  void checkout();
+  void updateRevenue();
 private:
   double total;
 };
